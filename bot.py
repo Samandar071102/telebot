fc3de2845f03cf01e560ec395bf764e389091216
Samandar071102 <125548961+Samandar071102@users.noreply.github.com>
2026-01-19 12:01:27 +0500

    Initialize Telegram bot with handlers and database

---
+++ bot.py
@@ -0,0 +1,32 @@
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, ConversationHandler, filters
+from config import BOT_TOKEN
+from db import init_db
+from parent_handlers import *
+from admin_handlers import *
+from states import *
+
+init_db()
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+# ota-ona
+app.add_handler(ConversationHandler(
+    entry_points=[CommandHandler("start", start_parent)],
+    states={
+        ADD_FIRST: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_first)],
+        ADD_LAST: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_last)],
+        ADD_GROUP: [CallbackQueryHandler(add_group)]
+    },
+    fallbacks=[]
+))
+
+# admin
+app.add_handler(CommandHandler("admin", admin_login))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, admin_password_check))
+
+app.add_handler(CommandHandler("start_lesson", start_lesson))
+app.add_handler(CallbackQueryHandler(choose_lesson_number, pattern="^grp_"))
+app.add_handler(CallbackQueryHandler(open_attendance_panel, pattern="^lesson_"))
+app.add_handler(CallbackQueryHandler(mark_attendance, pattern="^att_"))
+
+app.run_polling()
